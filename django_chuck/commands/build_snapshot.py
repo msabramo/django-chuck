143e86b454c77a83613d08390e6b442c93536101
Bastian Ballmann <bastian.ballmann@notch-interactive.com>
2012-06-05 12:25:49 +0200

    fixed build snapshot parser

--- django_chuck/commands/build_snapshot.py
+++ django_chuck/commands/build_snapshot.py
@@ -4,7 +4,7 @@ import os
 
 
 class Command(BaseCommand):
-    help = "Build a snapshot of the project requirements and post_build actions"
+    help = "Build a snapshot of the project requirements"
 
     def __init__(self):
         super(Command, self).__init__()
@@ -15,19 +15,24 @@ class Command(BaseCommand):
         self.print_header("BUILD SNAPSHOT")
         output = self.execute_in_project('pip freeze', return_result=True).split('\n')
 
+        import pudb; pudb.set_trace()
+
         self.requirements = {}
         for line in output:
             if line == '':
                 continue
-            index = line.find('=')
-            self.requirements[line[0:index]] = line[index:]
+            (app_name, version) = line.split("==")
+            self.requirements[app_name] = version
 
         self.update_requirement_file(os.path.join(self.site_dir, 'requirements/requirements_local.txt'))
         self.update_requirement_file(os.path.join(self.site_dir, 'requirements/requirements_dev.txt'))
         self.update_requirement_file(os.path.join(self.site_dir, 'requirements/requirements_live.txt'))
-        self.replace_requirement_file(os.path.join(self.site_dir, 'requirements/requirements.txt'))
+        self.update_requirement_file(os.path.join(self.site_dir, 'requirements/requirements.txt'))
 
     def update_requirement_file(self, file):
+        """
+        Replace requirements with explicit installed version
+        """
         for line in fileinput.input(file, inplace=1):
             if line.find('-r') >= 0 or line == '\n':
                 print line,
@@ -41,13 +46,4 @@ class Command(BaseCommand):
             else:
                 index = line.find('\n')
             app_name = line[0:index]
-            print "%s%s\n" % (app_name, self.requirements[app_name]),
-            del self.requirements[app_name]
-
-    def replace_requirement_file(self, file):
-        print "Updating " + file
-        f = open(file, 'w+')
-        f.truncate()
-        s = '\n'.join(['%s%s'% (app_name, self.requirements[app_name]) for app_name in self.requirements])
-        f.write(s)
-        f.close()
+            print "%s==%s\n" % (app_name, self.requirements[app_name]),
