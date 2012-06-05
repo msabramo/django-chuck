248067b71d5b65e20cdf2c191ec643082608d58f
Bastian Ballmann <bastian.ballmann@notch-interactive.com>
2012-06-05 10:25:47 +0200

    fixed build snapshot parser

--- django_chuck/commands/build_snapshot.py
+++ django_chuck/commands/build_snapshot.py
@@ -17,10 +17,12 @@ class Command(BaseCommand):
 
         self.requirements = {}
         for line in output:
-            if line == '':
+            if not "==" in line:
                 continue
-            index = line.find('=')
-            self.requirements[line[0:index]] = line[index:]
+
+            (app, version) = line.split("==")
+            self.requirements[app] = version
+
 
         self.update_requirement_file(os.path.join(self.site_dir, 'requirements/requirements_local.txt'))
         self.update_requirement_file(os.path.join(self.site_dir, 'requirements/requirements_dev.txt'))
@@ -29,20 +31,20 @@ class Command(BaseCommand):
 
     def update_requirement_file(self, file):
         for line in fileinput.input(file, inplace=1):
-            if line.find('-r') >= 0 or line == '\n':
-                print line,
-                continue
-            if line.find('>') > 0:
-                index = line.find('>')
-            elif line.find('<') > 0:
-                index = line.find('<')
-            elif line.find('=') > 0:
-                index = line.find('=')
+            if line.find('>=') > 0:
+                (app,version) = line.split('>=')
+            elif line.find('<=') > 0:
+                (app,version) = line.split('<=')
+            elif line.find('==') > 0:
+                (app,version) = line.split('==')
             else:
-                index = line.find('\n')
-            app_name = line[0:index]
-            print "%s%s\n" % (app_name, self.requirements[app_name]),
-            del self.requirements[app_name]
+                print line
+                continue
+
+            if self.requirements.get(app):
+                print "%s==%s\n" % (app, self.requirements[app]),
+                del self.requirements[app]
+
 
     def replace_requirement_file(self, file):
         f = open(file, 'w+')
